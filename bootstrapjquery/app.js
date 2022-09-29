4a0221df5562e9f789d59e37a9d7bb8a335f98ad
julienjego <jego.jj@gmail.com>
2022-09-29 14:59:34 +0200

    jquerify getScores()

--- bootstrapjquery/app.js
+++ bootstrapjquery/app.js
@@ -1,14 +1,16 @@
 $(document).ready(function () {
     $("#btn-search").click(() => {
         $.getJSON(
-            "https://fr.openfoodfacts.org/api/v0/product/3760020507350.json?fields=additives_original_tags,allergens,brands,categories,ecoscore_grade,image_front_url,ingredients_analysis_tags,ingredients_text_debug,ingredients_text_fr,ingredients_text_en,labels,nova_group,nutriscore_grade,nutrient_levels,nutriments,product_name,quantity",
+            "https://fr.openfoodfacts.org/api/v0/product/3033710065967.json?fields=additives_original_tags,allergens,brands,categories,ecoscore_grade,image_front_url,ingredients_analysis_tags,ingredients_text_debug,ingredients_text_fr,ingredients_text_en,labels,nova_group,nutriscore_grade,nutrient_levels,nutriments,product_name,quantity",
             (data) => {
                 console.log(data);
                 getProductInfo(data);
+                getScores(data);
             }
         );
     });
 
+    // On récupère les infos  pour la section "Le produit"
     function getProductInfo(data) {
         const productLbl = $("#product-name");
         const productQty = $("#product-quantity");
@@ -44,5 +46,70 @@ $(document).ready(function () {
         data.product.labels != null && data.product.labels.includes("Bio")
             ? bioLbl.css("display", "inline-block")
             : bioLbl.css("display", "none");
+    }
+
+    // On récupère les scores et on affiche les bonnes images
+    function getScores(data) {
+        const ecoScore = $("#ecoscore");
+        const nutriScore = $("#nutriscore");
+        const novaScore = $("#novascore");
+
+        // Obtenir et modifier l'éco-score
+        switch (data.product.ecoscore_grade) {
+            case "a":
+                ecoScore.attr("src", "../img/ecoscore-a.svg");
+                break;
+            case "b":
+                ecoScore.attr("src", "../img/ecoscore-b.svg");
+                break;
+            case "c":
+                ecoScore.attr("src", "../img/ecoscore-c.svg");
+                break;
+            case "d":
+                ecoScore.attr("src", "../img/ecoscore-d.svg");
+                break;
+            case "e":
+                ecoScore.attr("src", "../img/ecoscore-e.svg");
+                break;
+            default:
+                ecoScore.attr("src", "../img/ecoscore-na.svg");
+        }
+        // Obtenir et modifier le nutriscore
+        switch (data.product.nutriscore_grade) {
+            case "a":
+                nutriScore.attr("src", "../img/nutriscore-a.svg");
+                break;
+            case "b":
+                nutriScore.attr("src", "../img/nutriscore-b.svg");
+                break;
+            case "c":
+                nutriScore.attr("src", "../img/nutriscore-c.svg");
+                break;
+            case "d":
+                nutriScore.attr("src", "../img/nutriscore-d.svg");
+                break;
+            case "e":
+                nutriScore.attr("src", "../img/nutriscore-e.svg");
+                break;
+            default:
+                nutriScore.attr("src", "../img/nutriscore-na.svg");
+        }
+        // Obtenir et modifier le score nova
+        switch (data.product.nova_group) {
+            case 1:
+                novaScore.attr("src", "../img/nova-1.svg");
+                break;
+            case 2:
+                novaScore.attr("src", "../img/nova-2.svg");
+                break;
+            case 3:
+                novaScore.attr("src", "../img/nova-3.svg");
+                break;
+            case 4:
+                novaScore.attr("src", "../img/nova-4.svg");
+                break;
+            default:
+                novaScore.attr("src", "../img/nova-na.svg");
+        }
     }
 });
